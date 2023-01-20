6498b6943658f1e37bb5d7053c6f2b1c9e6bb0ae
Ratsim <ratsimhenintsoa@gmail.com>
2023-01-20 11:20:10 +0300

    commit lte getvoiture

--- express/Service/Fiche_Service.js
+++ express/Service/Fiche_Service.js
@@ -64,7 +64,7 @@ const ListeVoitureGarage = async (req, res) => {
         Fiche.find({
             user: new ObjectId(res.locals.user.id),
             etat: {
-                $gte: 3
+                $lte: 3
             }
         }).populate('user').populate('voiture').select().then(function (result) {
             if (result.length == 0) {
